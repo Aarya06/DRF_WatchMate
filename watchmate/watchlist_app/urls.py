ba9b3b9aec6fd4b2e03ec149ffe3918634ee5945
Aarya06 <raj.aarya06@gmail.com>
2022-06-18 18:07:20 +0530

    Basic setup with django

---
+++ watchmate/watchlist_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import movie_list, movie_details
+urlpatterns = [
+    path('list/',movie_list, name='Movie List'),
+    path('<int:pk>/',movie_details, name='Movie'),
+]
---
+++ watchmate/watchlist_app/views.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from .models import Movie
+
+def movie_list(req):
+    movies = Movie.objects.all()
+    response = {
+        'message': 'Fetched succesfully',
+        'movies': list(movies.values())
+    }
+    return JsonResponse(response)
+
+def movie_details(req, pk):
+    movie = Movie.objects.get(pk=pk)
+    response = {
+        'message': 'Fetched succesfully',
+        'name': movie.name,
+        'description': movie.description,
+        'is_active': movie.is_active
+    }
+    return JsonResponse(response)
